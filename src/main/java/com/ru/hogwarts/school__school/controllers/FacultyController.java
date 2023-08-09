9ba0c113ab743432123ff78ebe9acccb6dce8add
Elena <elenela@yandex.ru>
2023-08-09 15:29:54 +0300

    Д.з. к  уроку 4.1. продолжение SQL2

--- src/main/java/com/ru/hogwarts/school__school/controllers/FacultyController.java
+++ src/main/java/com/ru/hogwarts/school__school/controllers/FacultyController.java
@@ -1,44 +1,81 @@
+
 package com.ru.hogwarts.school__school.controllers;
+
 import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.*;
+
 import com.ru.hogwarts.school__school.models.Faculty;
+
 import com.ru.hogwarts.school__school.services.FacultyService;
+
 import java.util.Collection;
+
 @RestController
+
 @RequestMapping("/faculty")
-  public class FacultyController{
-        private final FacultyService facultyService;
+
+public class FacultyController{
+
+       private final FacultyService facultyService;
+
         public FacultyController(FacultyService facultyService){
         this.facultyService=facultyService;
         }
+
        @GetMapping("{id}")
+
         public ResponseEntity<Faculty> getFacultyInfo(@PathVariable Long id){
+
         Faculty faculty=facultyService.findFaculty(id);
+
         if(faculty==null){
+
         return ResponseEntity.notFound().build();
         }
+
         return ResponseEntity.ok(faculty);
         }
-@GetMapping
+
+        @GetMapping
+
         public ResponseEntity<Collection<Faculty>>getAllFacultyInfo(){
-        return ResponseEntity.ok(facultyService.getAllFaculties());
+
+            return ResponseEntity.ok(facultyService.getAllFaculties());
+
         }
-@PostMapping
+
+        @PostMapping
+
         public Faculty createFaculty(@RequestBody Faculty faculty){
         return facultyService.addFaculty(faculty);
         }
-@PutMapping
+
+        @PutMapping
+
         public ResponseEntity<Faculty> editFaculty(@RequestBody Faculty faculty){
-        Faculty foundFaculty=facultyService.editFaculty(faculty);
-        if(foundFaculty==null){
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
+
+            Faculty foundFaculty=facultyService.editFaculty(faculty);
+
+            if(foundFaculty==null){
+
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+
+            }
+
         return ResponseEntity.ok(foundFaculty);
+
         }
-@DeleteMapping("{id}")
+
+        @DeleteMapping("{id}")
+
         public ResponseEntity<Faculty> deleteFaculty(@PathVariable Long id){
+
         facultyService.deleteFaculty(id);
+
         return ResponseEntity.ok().build();
+
         }
 }
--- src/main/java/com/ru/hogwarts/school__school/repositories/FacultyRepository.java
+++ src/main/java/com/ru/hogwarts/school__school/repositories/FacultyRepository.java
@@ -1,10 +1,16 @@
+
 package com.ru.hogwarts.school__school.repositories;
+
 import com.ru.hogwarts.school__school.models.Faculty;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
+
 @Repository
+
 public interface FacultyRepository  extends JpaRepository<Faculty, Long> {
-        Collection<Faculty> findFacultiesByNameOrColorIgnoreCase(String name, String color);
+
+       Collection<Faculty> findFacultiesByNameOrColorIgnoreCase(String name, String color);
+
     }
