554475f5c6d2485920cf2b546b66fe9884703552
Elena <elenela@yandex.ru>
2023-08-17 12:02:44 +0300

    Д.з. к  уроку 4.6.   Потоки

--- src/main/java/com/ru/hogwarts/school__school/services/FlowInOrderService.java
+++ src/main/java/com/ru/hogwarts/school__school/services/FlowInOrderService.java
@@ -3,39 +3,59 @@ package com.ru.hogwarts.school__school.services;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+
+import static java.nio.file.Paths.get;
+
 @Service
 public class FlowInOrderService {
 
-    public void   studentsAreListedInOrder() {
-        new FlowInOrderService ().work();
+    public void studentsAreListedInOrder() {
+        new FlowInOrderService().work();
     }
 
     private void work() {
-        List<String> students  = Arrays.asList("Рон", "Гермиона","Малфой","Падма","Гарри","Алланис");
-        print (students);
-        print (students);
-        new  Thread (()-> {
+        List<String> students = Arrays.asList("Рон", "Гермиона", "Малфой", "Падма", "Гарри", "Алланис");
 
-            print(students.get(2), students.get(3));
+        print1(students);
+        print1(students);
 
 
-        } ).start();
-        new Thread(()-> {
-            print(students.get(4), students.get(5));
+        new Thread(() -> {
+            try{
+                Thread.sleep(2000);
+
+            }
+            catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+// даже если мы в средний поток добавим .sleep  - последовательность все равно будет выполняться
+// если мы работаем только с индексом  int index;
+            print1(students);
 
 
-        } ).start();
+            print1(students);
+
+
+        }).start();
+
+        new Thread(() -> {
+
+            print1(students);
+
+
+            print1(students);
+
+
+        }).start();
+
+
+
     }
 
-
-    public synchronized void print (String student1, String student2){
-        System.out.println(student1);
-        System.out.println(student2);
-
-    }
     int index;
-    private void print(List<String> students) {
+    private  synchronized  void print1(List<String> students) {
         System.out.println(students.get(index++));
     }
 }
